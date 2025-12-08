9c114aff734e88ccb77dc817abae5b18ebb15192
CDOOR <CDOOR@CHUGIN>
2025-12-08 16:29:14 +0500

    .

--- Tyuiu.ChuginNM.Sprint5.Task2.V7.Lib/DataService.cs
+++ Tyuiu.ChuginNM.Sprint5.Task2.V7.Lib/DataService.cs
@@ -17,14 +17,9 @@ namespace Tyuiu.ChuginNM.Sprint5.Task2.V7.Lib
 
             string res = "";
 
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    res += matrix[i, j];
-                }
-
-            }
+            res += matrix[0, 0] + ";" + matrix[0, 1] + ";" + matrix[0, 2] + "\n"
+                + matrix[1, 0] + ";" + matrix[1, 1] + ";" + matrix[1, 2] + "\n"
+                + matrix[2, 0] + ";" + matrix[2, 1] + ";" + matrix[2, 2] + "\n";
 
             return res;
 
