0c6190a2add1c4279ce16838bc9cdecaec1bbefb
CDOOR <CDOOR@CHUGIN>
2025-12-09 21:12:59 +0500

    .

--- Tyuiu.ChuginNM.Sprint5.Task4.V1.Lib/DataService.cs
+++ Tyuiu.ChuginNM.Sprint5.Task4.V1.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.ChuginNM.Sprint5.Task4.V1.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
-            double x = Convert.ToDouble(strx);
+            double x = double.Parse(strx);
             double y = Math.Round(((1 / (Math.Cos(x) + x)) - 4.12 * x), 3);
             return y;
         }
