f8c66368ca74faf7bad8a4997ca56e3752ed2c3f
CDOOR <CDOOR@CHUGIN>
2025-12-10 00:45:43 +0500

    .

--- Tyuiu.ChuginNM.Sprint5.Task7.V3.Lib/DataService.cs
+++ Tyuiu.ChuginNM.Sprint5.Task7.V3.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.ChuginNM.Sprint5.Task7.V3.Lib
     {
         public string LoadDataAndSave(string path)
         {
-            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}/OutPutDataFileTask7V3.txt"; ;
+            string pathSaveFile = Path.Combine(Path.GetTempPath(),@"OutPutDataFileTask7V3.txt");
             FileInfo fileinfo = new FileInfo(pathSaveFile);
             bool fileexists = fileinfo.Exists;
 
